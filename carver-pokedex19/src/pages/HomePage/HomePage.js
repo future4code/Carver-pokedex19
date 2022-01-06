631fc24538a9a9e169ed2bbc26c16a7a63467d16
Mayra <mb509311@gmail.com>
2022-01-06 21:33:17 -0200

    lista dos pokemons

--- carver-pokedex19/src/pages/HomePage/HomePage.js
+++ carver-pokedex19/src/pages/HomePage/HomePage.js
@@ -1,11 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
+import {CardContainer, CardFoto, FormContainer} from './styled'
+import Button from '@material-ui/core/Button';
 
-const HomePage = () => {
+
+export default function HomePage() {
+  const [pokemons, setPokemons]= useState([]);
+  
+useEffect(() => {
+  cardPokemon()
+}, []);
+
+  const cardPokemon = () => {
+    const list=[];
+    for(let i = 1; i <= 20; i++){
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`)
+    
+    .then((res) => {
+      list.push(res.data)
+      if(list.length===20){
+      setPokemons(list)
+      }
+      console.log('Details', pokemons)
+
+    })
+    .catch((err) => 
+    console.log(err.message))
+    }
+};
+
+const details = pokemons && pokemons.map((pokemon) => {
+  return <CardContainer>
+  <h2>{pokemon.name}</h2>
+  <CardFoto>
+  <img src={pokemon.sprites.front_default} alt=''/>
+  </CardFoto>
+  <Button>Adicionar a Pokédex</Button>
+  <Button>Ver Detalhes</Button>
+  </CardContainer>
+  
+})
+
   return (
-    <div>
-      HomePage
-    </div>
+      <FormContainer>
+      <h2>Lista de Pokémons</h2>
+      {details}
+      </FormContainer>
+      
+
   );
 }
 
-export default HomePage;
---
+++ carver-pokedex19/src/pages/HomePage/styled.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+
+export const CardContainer = styled.div`
+ display:flex;
+ flex-wrap:wrap;
+ margin:30px;
+ display:flex;
+ flex-direction: column; 
+ flex-direction: row;
+ justify-content: space-between;
+`
+export const CardFoto = styled.div`
+width: 64%;
+`
+export const FormContainer = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+margin-bottom: -30px;
+margin-top: 10px;
+color: navy;
+
+
+`
